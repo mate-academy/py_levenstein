32925da8ee2f0df9d5d9989e02693b02fde5d49f
Platoniuks <platon@alterpost.org>
2019-12-03 08:19:29 +0200

    i am sure now

--- levenstein.py
+++ levenstein.py
@@ -1,4 +1,6 @@
 """Levenstein distance"""
+import numpy
+
 
 def dist(first_string: str, second_string: str) -> int:
     """
@@ -7,13 +9,22 @@ def dist(first_string: str, second_string: str) -> int:
     :param second_string: second string
     :return: Levenstein distance
     """
-    distance = 0
-    if len(first_string) != len(second_string):
-        longest_string = max(first_string, second_string, key=len)
-        smallest_string = min(first_string, second_string, key=len)
-    else:
-        longest_string, smallest_string = first_string, second_string
-    for i in longest_string:
-        if i not in smallest_string:
-            distance += 1
-    return distance
+    rows = len(first_string) + 1
+    cols = len(second_string) + 1
+    distance = numpy.zeros((rows, cols), dtype=int)
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            distance[i][0] = i
+            distance[0][j] = j
+
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if first_string[row-1] == second_string[col-1]:
+                cost = 0
+            else:
+                cost = 1
+            distance[row][col] = min(distance[row-1][col] + 1,
+                                     distance[row][col-1] + 1,
+                                     distance[row-1][col-1] + cost)
+    return distance[row][col]
