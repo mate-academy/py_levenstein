eeb79ab83248308308b2aab1390cb794294b3601
Andrii <andrewsotnikov88@gmail.com>
2019-12-04 21:57:26 +0200

    Levenstein distance solution

--- levenstein.py
+++ levenstein.py
@@ -1,2 +1,15 @@
-def dist(s1: str, s2: str) -> int:
-    return 0
+"""Write a function to calculate the Levenstein distance"""
+
+
+def dist(string_one: str, string_two: str) -> int:
+    """Return the Levenshtein edit distance between two strings"""
+    if not string_one:
+        return len(string_two)
+    if not string_two:
+        return len(string_one)
+    if string_one[0] == string_two[0]:
+        return dist(string_one[1:], string_two[1:])
+    first = dist(string_one, string_two[1:])
+    second = dist(string_one[1:], string_two)
+    third = dist(string_one[1:], string_two[1:])
+    return min(first, second, third) + 1
